a9a68515d34f08e6e877189fb21260b8266cb938
jack-carling <jack@carling.se>
2021-09-20 16:17:39 +0200

    Update server.js
    • Added console.log for listening port

--- server.js
+++ server.js
@@ -23,40 +23,43 @@ app.post('/api/tasks', (require, response) => {
   if (require.body.id !== undefined) {
     completeData(require.body.id);
   }
-  response.json({success: true});
+  response.json({ success: true });
 });
 
 app.delete('/api/tasks/:id', (require, response) => {
   deleteData(require.params.id);
-  response.json({success: true});
+  response.json({ success: true });
 });
 
 function addData(inputTask) {
   const checkDatabase = database.getState();
   let databaseElem;
   let newNumber;
-  if (checkDatabase.tasks.length > 0) { //Om databasen är tom börjar ID nummer om från 1, annars hämta sista
+  if (checkDatabase.tasks.length > 0) {
+    //Om databasen är tom börjar ID nummer om från 1, annars hämta sista
     databaseElem = database.get('tasks').takeRight(1).value();
     newNumber = databaseElem[0].id + 1;
   } else {
     newNumber = 1;
   }
-  database.get('tasks').push({id: newNumber, task: inputTask, completed: false}).write();
+  database.get('tasks').push({ id: newNumber, task: inputTask, completed: false }).write();
 }
 
 function completeData(inputID) {
   inputID = Number(inputID);
-  const search = database.get('tasks').find({id: inputID}).value();
+  const search = database.get('tasks').find({ id: inputID }).value();
   if (search.completed === true) {
-    database.get('tasks').find({ id: inputID}).assign({ completed: false}).write();
+    database.get('tasks').find({ id: inputID }).assign({ completed: false }).write();
   } else {
-    database.get('tasks').find({ id: inputID}).assign({ completed: true}).write();
+    database.get('tasks').find({ id: inputID }).assign({ completed: true }).write();
   }
 }
 
 function deleteData(deleteID) {
- deleteID = Number(deleteID);
- const search = database.get('tasks').remove({id: deleteID}).write();
+  deleteID = Number(deleteID);
+  const search = database.get('tasks').remove({ id: deleteID }).write();
 }
 
-app.listen(port);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
